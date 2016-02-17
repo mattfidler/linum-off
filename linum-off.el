3e37baaad27d27e405f8dfe01d4ab9cd5b591353
Matthew Fidler <matthew.fidler@gmail.com>
2016-02-17 15:37:44 -0600

    Update linum-off.el

--- linum-off.el
+++ linum-off.el
@@ -62,7 +62,7 @@
 ;;; Code:
 (require 'linum)
 
-(defcustom linum-disabled-modes-list '(eshell-mode wl-summary-mode compilation-mode org-mode text-mode dired-mode)
+(defcustom linum-disabled-modes-list '(eshell-mode wl-summary-mode compilation-mode org-mode text-mode dired-mode pdf-view-mode)
   "* List of modes disabled when global linum mode is on"
   :type '(repeat (sexp :tag "Major mode"))
   :tag " Major modes where linum is disabled: "
